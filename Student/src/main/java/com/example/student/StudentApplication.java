c2fb29b9a3de570b4d2fee98fce88117279b18e2
theknownn <theknownndev@gmail.com>
2026-03-02 20:23:49 +0530

    Added Docker file

--- Student/src/main/java/com/example/student/StudentApplication.java
+++ Student/src/main/java/com/example/student/StudentApplication.java
@@ -2,7 +2,7 @@ package com.example.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+///main app
 @SpringBootApplication
 public class StudentApplication {
 
